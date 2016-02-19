f22e1e94f7652fefd3f3a43c36f3152df4446426
fhergomez <fhergomez37@gmail.com>
2016-02-19 09:09:17 -0800

    added profile and error message

---
+++ Node.js_basics/profile.js
@@ -0,0 +1,48 @@
+// Problem: we need a simple way to look at a user's badge count and JS points
+// Solution: use node.js to connect to treehouse's API to get profile information to print out
+
+var https = require("https");
+var http = require("http");
+
+
+// print out message
+function printMessage(username, badgeCount, points) {
+  var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
+  console.log(message);
+};
+
+//print out error messages
+function printError(error) {
+  console.error(error.message);
+}
+
+// connect to the API URL (://teamtreehouse.com/username.json)
+var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response){
+    var body = "";
+    // read the data
+    response.on("data", function(data){
+      body += data;
+    });
+    response.on("end", function(){
+      if(response.statusCode === 200) {
+        try {
+          var profile = JSON.parse(body);
+          printMessage(username, profile.badges.length, profile.points.JavaScript);
+          // console.dir(profile);
+        } catch(error) {
+          // parse error
+          printError(error);
+        }
+      } else {
+        // Status Code
+        printError({message: "There was an error getting the profile for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"});
+      }
+    });
+});
+
+// Connection error
+request.on("error", printError);
+
+
+// parse the data
+// print the data
