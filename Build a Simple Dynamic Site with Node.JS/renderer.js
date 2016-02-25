a365872a9884fea032415b46c45bbce755770550
fhergomez <fhergomez37@gmail.com>
2016-02-24 23:38:20 -0800

    fixed http problem

--- Build a Simple Dynamic Site with Node.JS/renderer.js
+++ Build a Simple Dynamic Site with Node.JS/renderer.js
@@ -2,8 +2,13 @@ var fs = require('fs');
 
 function view(templateName, values, response) {
   // Read from the template file
+  fs.readFile('./views/' + templateName + '.html', function(error, fileContents) {
+    if (err) throw err;
+    // Insert values in to the content
 
-  // Insert values in to the content
-
-  // write out to the response
+    // write out to the response
+    response.write(fileContents);
+  });
 }
+
+module.exports.view = view;
--- Build a Simple Dynamic Site with Node.JS/router.js
+++ Build a Simple Dynamic Site with Node.JS/router.js
@@ -1,12 +1,13 @@
 var Profile = require("./profile.js");
+var renderer = require("./renderer.js")
 
 //Handle HTTP route GET / and POST / i.e. Home
 function home(request, response) {
   //if url == "/" && GET
   if(request.url === "/") {
     //show search
-    response.writeHead(200, {'Content-Type': 'text/plain'});  
-    response.write("Header\n");
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    renderer.view("header", {}, response);
     response.write("Search\n");
     response.end('Footer\n');
   }
@@ -19,18 +20,18 @@ function user(request, response) {
   //if url == "/...."
   var username = request.url.replace("/", "");
   if(username.length > 0) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});  
+    response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Header\n");
-    
+
     //get json from Treehouse
     var studentProfile = new Profile(username);
     //on "end"
     studentProfile.on("end", function(profileJSON){
       //show profile
-      
+
       //Store the values which we need
       var values = {
-        avatarUrl: profileJSON.gravatar_url, 
+        avatarUrl: profileJSON.gravatar_url,
         username: profileJSON.profile_name,
         badges: profileJSON.badges.length,
         javascriptPoints: profileJSON.points.JavaScript
@@ -39,14 +40,14 @@ function user(request, response) {
       response.write(values.username + " has " + values.badges + " badges\n");
       response.end('Footer\n');
     });
-        
+
     //on "error"
     studentProfile.on("error", function(error){
       //show error
       response.write(error.message + "\n");
       response.end('Footer\n');
     });
-      
+
   }
 }
 
