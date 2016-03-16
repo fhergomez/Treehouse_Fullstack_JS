3e4bf2489cc771affd2a43dc7cd9758436150e9a
fhergomez <fhergomez37@gmail.com>
2016-03-16 00:34:33 -0700

    changed posts

---
+++ express-basics/src/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var express = require('express'),
+      posts = require('./mock/posts.json');
+
+var app = express();
+
+app.get('/',function(req,res){
+  res.send('<h1>I am loving Treehouse a lot</h1>');
+})
+
+app.get('/blog/:title',function(req,res){
+  var title = req.params.title;
+  var post = post[title];
+  res.send(post);
+});
+
+app.listen(3000, function(){
+  console.log("The frontend server is running on port 3000!");
+});
