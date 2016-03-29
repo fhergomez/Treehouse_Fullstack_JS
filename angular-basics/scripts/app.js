79dfbb2df4288ea1bdd6d259d5e2107ee93668d8
fhergomez <fhergomez37@gmail.com>
2016-03-28 19:31:07 -0700

    add controllers to app.js

--- angular-basics/scripts/app.js
+++ angular-basics/scripts/app.js
@@ -1,1 +1,7 @@
-angular.module('todoListApp', []);
+var app = angular.module('todoListApp', []);
+
+app.controller('mainCtrl', ['$scope', function ($scope) {
+  $scope.helloWorld = function(){
+    console.log('This the helloWorld controller function in the mainCtrl!');
+  }
+}]);
