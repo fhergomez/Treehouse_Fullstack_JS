f38f073c93a63216773f81889f557fd22f212ae0
fhergomez <fhergomez37@gmail.com>
2016-03-30 00:40:44 -0700

    add todos

--- angular-basics/scripts/app.js
+++ angular-basics/scripts/app.js
@@ -4,10 +4,12 @@ app.controller('mainCtrl', ['$scope', function ($scope) {
   $scope.helloWorld = function(){
     console.log('This the helloWorld controller function in the mainCtrl!');
   }
-}]);
 
-app.controller('coolCtrl', ['$scope', function ($scope) {
-  $scope.whoAmI = function(){
-    console.log('Hello there, this is the coolCtrl function!')
-  }
-}])
+  $scope.todos = [
+    {name: 'clean the house'},
+    {name: 'water the dog'},
+    {name: 'code at least 2 hr per day'},
+    {name: 'wash the dishes'},
+    {name: 'email nicole'}
+  ]
+}]);
