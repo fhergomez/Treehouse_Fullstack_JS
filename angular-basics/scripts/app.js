3fa8d77038c6c97017d3104a4ecb6fe2ed23e229
fhergomez <fhergomez37@gmail.com>
2016-04-03 23:18:23 -0700

    add ng-class and ng-change

--- angular-basics/scripts/app.js
+++ angular-basics/scripts/app.js
@@ -1,8 +1,8 @@
 var app = angular.module('todoListApp', []);
 
 app.controller('mainCtrl', ['$scope', function ($scope) {
-  $scope.helloWorld = function(){
-    console.log('This the helloWorld controller function in the mainCtrl!');
+  $scope.learningNgChange = function(){
+    console.log('An input changed!');
   }
 
   $scope.todos = [
