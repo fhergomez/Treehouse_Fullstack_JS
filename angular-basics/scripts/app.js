7cf2adbd1fa3981a7094653ffa307b84b308a49a
fhergomez <fhergomez37@gmail.com>
2016-04-04 12:31:27 -0700

    add service

--- angular-basics/scripts/app.js
+++ angular-basics/scripts/app.js
@@ -1,15 +1,28 @@
 var app = angular.module('todoListApp', []);
 
-app.controller('mainCtrl', ['$scope', function ($scope) {
+app.controller('mainCtrl', function ($scope,dataService) {
+  $scope.helloConsole = dataService.helloConsole;
+
+  dataService.getTodos(function(response){
+    console.log(response.data);
+    $scope.todos = response.data;
+  });
+
   $scope.learningNgChange = function(){
     console.log('An input changed!');
   }
 
-  $scope.todos = [
-    {name: 'clean the house'},
-    {name: 'water the dog'},
-    {name: 'code at least 2 hr per day'},
-    {name: 'wash the dishes'},
-    {name: 'email nicole'}
-  ]
-}]);
+})
+
+.service('dataService', function ($http) {
+  this.helloConsole = function(){
+    console.log("This is the Hello Console Service!")
+  };
+
+  this.getTodos = function(callback){
+    $http({
+      method: 'GET',
+      url: 'mock/todos.json'
+    }).then(callback)
+  };
+});
